42863d9b493f6a2421ba00e423e7bf65aa15435f
navyashetti50041-rgb <navyashetti50041@gmail.com>
2026-05-08 21:29:58 +0530

    Implement voting eligibility check with exception handling

---
+++ VotingEligibility.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+class NotEligible extends Exception {
+    int age;
+    NotEligible(int age) {
+        this.age = age;
+    }
+    public String toString() {
+        return "Age " + age + " is not eligible for voting";
+    }
+}
+public class votingEligibility {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+        try {
+            if (age < 18) {
+                throw new NotEligible(age);   // ❌ exception
+            } 
+            else {
+                System.out.println("Eligible for voting");
+            }
+        } 
+        catch (NotEligible ne) {
+            System.out.println(ne);
+        } 
+        finally {
+        	sc.close();
+            System.out.println("Exiting!!");
+        }
+    }
+}
